5520bf9be819d9f0c00df8d6ef4578d9bb05f2a0
John Ivan Mangaran <John Ivan Mangaran>
2019-06-19 10:01:12 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+(function($) {
+    'use strict';
+
+
+$(document).ready(function(event) {
+    $('.owl_projects').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:false,
+        // center: true,
+        smartSpeed:800,
+        // animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        dotsEach: 3,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            575:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+
+});
+
+
+
+jQuery(document).ready(function(){
+    jQuery('.skill_container').each(function(){
+        jQuery(this).find('.skills').animate({
+            width:jQuery(this).attr('data-percent')
+        },3000);
+    });
+});
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'linear',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+
+
+
+
+
+})(jQuery);
